a49dec3914f51c371c962ce287d276fe7ed5b7df
Yunseop Kim <undefined@mealant.com>
2018-10-25 22:26:40 +0900

    separate the util functions

--- src/util.js
+++ src/util.js
@@ -1,2 +1,17 @@
 const isOdd = (n) => Math.abs(n % 2) === 1;
 const isEven = (n) => (n % 2) === 0;
+function chunk(array, size) {
+    const result = [];
+    let index = 0;
+    while (index < array.length) {
+        result.push(array.slice(index, size + index));
+        index += size;
+    }
+    return result;
+}
+
+export {
+    isOdd,
+    isEven,
+    chunk
+}
