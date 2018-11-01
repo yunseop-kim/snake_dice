eb3c95f6099744cd4be4660caa03534c88cdcdbc
Yunseop Kim <undefined@mealant.com>
2018-11-02 00:25:09 +0900

    webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        hotOnly: true,
+        stats: 'errors-only'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Snake Dice Game'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
