876ca9bc20bfd1d8dda59545ef86c89400547d4e
Yunseop Kim <undefined@mealant.com>
2018-11-06 21:34:52 +0900

    add flatten

--- src/util.js
+++ src/util.js
@@ -10,8 +10,26 @@ function chunk(array, size) {
     return result;
 }
 
+function flatten(input) {
+    const stack = [...input];
+    const res = [];
+    while (stack.length) {
+        // 스택에서 값을 pop
+        const next = stack.pop();
+        if (Array.isArray(next)) {
+            // 배열 항목을 다시 push, 원래 인풋을 수정하지 않음
+            stack.push(...next);
+        } else {
+            res.push(next);
+        }
+    }
+    //입력 순서를 복구하기 위한 reverse
+    return res.reverse();
+}
+
 export {
     isOdd,
     isEven,
-    chunk
+    chunk,
+    flatten
 }
