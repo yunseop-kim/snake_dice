fc58daf75edd1355e9d11e48ccc59b80f936199c
Yunseop Kim <undefined@mealant.com>
2018-11-06 21:35:09 +0900

    append cells

--- src/Board.js
+++ src/Board.js
@@ -1,20 +1,50 @@
-import { chunk } from './util.js'
+import {
+    chunk,
+    flatten
+} from './util.js'
 export default class Board {
     constructor(n = 4) {
         if (typeof n !== "number") throw 'n is not a number!';
-        this._n = n;
+        this.array = this._generateZigzag(n)
     }
 
-    render() {
-        const result = chunk([...Array(this._n * this._n).keys()], this._n)
+    _generateZigzag(n) {
+        return chunk([...Array(n * n).keys()], n)
             .map((rows, index) => {
                 if (index % 2 === 1) rows.reverse();
                 return rows
             })
-        console.log('result', result)
-        // const cells = document.createElement('div')
-        // const node = document.createTextNode('This is Test')
-        // cells.appendChild(node);
-        // document.getElementById('app').appendChild(cells)
+    }
+
+    _rows() {
+        this.array.map((rows, index) => {
+            const divRow = document.createElement('div')
+            divRow.classList.add('row')
+            divRow.setAttribute('id', 'row' + index)
+            // div.innerHTML = rows
+            rows.map(cell => {
+                const divCell = document.createElement('div')
+                divCell.classList.add('cell')
+                divCell.innerHTML = cell
+                divRow.appendChild(divCell)
+                return divCell
+            })
+            return divRow
+        }).map(div => document.getElementById('app').appendChild(div))
+    }
+
+// _rows() {
+//     this.array.map((row, index) => {
+//         const div = document.createElement('div')
+//         div.classList.add('row')
+//         div.setAttribute('id', 'row' + index)
+//         // div.innerHTML = row
+
+//         return div
+//     }).map(div => document.getElementById('app').appendChild(div))
+// }
+
+    render() {
+        this._rows()
     }
 }
